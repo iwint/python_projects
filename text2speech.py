c37f0b38eef9821e060814f61ebf7ef1d101a20c
iwint <iwint.cseasnsct@gmail.com>
2022-09-05 06:38:50 +0530

    first commit

---
+++ text2speech.py
@@ -0,0 +1,19 @@
+import imp
+from newspaper import Article
+import nltk
+from gtts import gTTS
+import os
+
+article = Article('https://www.geeksforgeeks.org/machine-learning/?ref=shm')
+article.download()
+article.parse()
+
+nltk.download('punkt')
+article.nlp()
+
+mytext = article.text
+language = 'en'
+
+myobj = gTTS(text=mytext,lang=language,slow=False)
+myobj.save("read_article.mp3")
+os.system("start read_article.mp3")
